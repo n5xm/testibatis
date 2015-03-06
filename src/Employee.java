a6c96ec7ad7dc2a8a32239311270935581da7a3d
n5xm <evangel_a@163.com>
2015-03-06 08:47:44 +0800

    Complete ibatis_update_operation

--- src/Employee.java
+++ src/Employee.java
@@ -14,20 +14,36 @@ public class Employee {
 		this.salary = salary;
 	}
 
-	/* Here are the method definitions */
+	/* Here are the required method definitions */
 	public int getId() {
 		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getFirstName() {
 		return first_name;
 	}
 
+	public void setFirstName(String fname) {
+		this.first_name = fname;
+	}
+
 	public String getLastName() {
 		return last_name;
 	}
 
+	public void setlastName(String lname) {
+		this.last_name = lname;
+	}
+
 	public int getSalary() {
 		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
 	}
 } /* End of Employee */
